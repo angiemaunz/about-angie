9535ecdaa8e1523741e45c1c5a9d7f5c68afed6a
Humberto Perez <ferdinero121312@gmail.com>
2015-07-17 05:17:34 -0700

    I changed Angie's about me

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <! DOCTYPE html>
 <hmtl>
 
-	<title>Data Design Proyect </title>
-	<body> She is a nice lady
-	<p> She likes animals</p>
+	<title>Data Design Proyect Phase 1 </title>
+	<body>
+		<header>
+			<h1> JOSE SANCHEZ</h1>
+		</header>
+	<p> HE is school teacher. He likes extreme sport. HE likes speed. He is single with one kid and one dog. He travels in the summer.</p>
 	<img src="images/actionfoto.jpg" alt="action"
+
+		  <p> </p>
 	</body>
 </hmtl>
