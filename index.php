05ad6a1837f310ee0324422f7459f82d17cf2431
Humberto Perez <ferdinero121312@gmail.com>
2015-07-17 06:26:27 -0700

    I changed Angie's about me

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 	<p> HE is school teacher. He likes extreme sport. HE likes speed. He is single with one kid and one dog. He travels in the summer.</p>
 	<img src="images/actionfoto.jpg" alt="action"
 
-		  <p> </p>
+		  <p>the first use case is A person rent a car by accesing the site, deciding the price and clicking.</p>
+			<p>The  second use case is A person rent a car  by accessing  the site,reserve the car later rent the car</p>
+			<p> The third use case is a person try to rent a car by accesing the site make reservation but never reserve it </p>
+			<h1>Entity Relationship Diagram</h1>
+		<img src="images/grafica1.jpg"alt="diagram"
 	</body>
 </hmtl>
